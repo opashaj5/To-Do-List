f5656fbf272d2ae31e78486e5949c74f41857736
opashaj5 <opashaj5@yahoo.com>
2022-04-19 20:20:40 -0500

    add divs to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Index Page</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div id="to-do" className="section">
+          <h2>My To Do List:</h2>
+          <p className="new-item">New Item</p>
+          <form onSubmit={handleSubmit}>
+            <input placeholder='Enter new item' type="text" ref={entry} />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
