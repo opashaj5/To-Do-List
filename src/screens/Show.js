6a72b21f9ea4b400ad81e1ca78d9187b719be313
opashaj5 <opashaj5@yahoo.com>
2022-04-19 22:11:17 -0500

    delete README.md and App.css content, create Show.js

--- src/screens/Show.js
+++ src/screens/Show.js
@@ -1,5 +1,43 @@
-function Show() {
-    return <div>Show Page</div>;
+import React, { useState, useEffect } from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const Show = () => {
+    const { id } = useParams()
+    const [show, setShow] = useState({})
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/tasks/${id}`)
+                setShow(response.data)
+            } catch (err) {
+                console.log(err)
+            }
+        })()
+    }, [])
+
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`http://localhost:3000/tasks/${id}`)
+        } catch (err) {
+            console.log()
+        } finally {
+            navigate(-1)
+        }
+    }
+
+    return (
+        <div className="showPage">
+            <Link to="/"><button className='button'>Home</button></Link>
+            <div className="taskContainer">
+                <h1>Entry: {show.entry}</h1>
+                <p>Status: {show.status}</p>
+                <button onClick={handleDelete}>Delete</button>
+            </div>
+        </div>
+    )
 }
 
 export default Show;
