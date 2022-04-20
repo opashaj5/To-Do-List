b167b2e1c614353a328d58ada29101899effc306
opashaj5 <opashaj5@yahoo.com>
2022-04-19 20:05:27 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path="/tasks" element={<Index />} />
+      </Route>
+    </Routes>
+  </Router>
+  ,
+  document.getElementById('root')
 );
