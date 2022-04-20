9769a9eada6a0ace3c2d87fe3ee450517ab91e15
opashaj5 <opashaj5@yahoo.com>
2022-04-19 20:11:32 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css';
+import Show from './screens/Show';
+import Layout from './screens/layout/Layout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<App />} />
-        <Route path="/tasks" element={<Index />} />
+        <Route path=":id" element={<Show />} />
       </Route>
     </Routes>
-  </Router>
-  ,
+  </Router>,
   document.getElementById('root')
 );
